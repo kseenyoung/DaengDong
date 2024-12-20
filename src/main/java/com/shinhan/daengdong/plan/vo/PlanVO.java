270b35a11118e2e71173881661f6189afeb5fd83
HongEC <honlucete@naver.com>
2024-12-20 14:15:23 +0900

    feat: Plan 폼 작성,

---
+++ src/main/java/com/shinhan/daengdong/plan/vo/PlanVO.java
@@ -0,0 +1,13 @@
+package com.shinhan.daengdong.plan.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Setter @Getter
+@ToString
+public class PlanVO {
+
+}
