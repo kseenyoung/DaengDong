6ce87b10cb0136e57242feead0ddc4ea3847edef
HongEC <honlucete@naver.com>
2024-12-23 11:24:42 +0900

    feat: Plan info 틀 작성

--- src/main/java/com/shinhan/daengdong/plan/controller/PlanController.java
+++ src/main/java/com/shinhan/daengdong/plan/controller/PlanController.java
@@ -3,7 +3,6 @@ package com.shinhan.daengdong.plan.controller;
 import com.shinhan.daengdong.plan.dto.PlanDTO;
 import com.shinhan.daengdong.plan.model.service.PlanServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
