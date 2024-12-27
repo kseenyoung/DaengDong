3138dfe1422398572984086213d672c4570a790a
minyoung <jminy0302@gmail.com>
2024-12-27 11:54:46 +0900

    feat: KakaoAPI를 활용하여 place에 search기능 추가, category search 기능 추가, 수정중

---
+++ src/main/java/com/shinhan/daengdong/plan/controller/SearchPlaceController.java
@@ -0,0 +1,26 @@
+package com.shinhan.daengdong.plan.controller;
+
+import com.shinhan.daengdong.plan.dto.PlanDTO;
+import com.shinhan.daengdong.plan.model.service.PlanServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/search")
+public class SearchPlaceController {
+
+    @Autowired
+    private PlanServiceInterface planService;
+
+    @GetMapping("/place")
+    public String searchPlaceForm(Model model) {
+        return "place/searchPlace"; // searchPlace.jsp
+    }
+
+
+}
