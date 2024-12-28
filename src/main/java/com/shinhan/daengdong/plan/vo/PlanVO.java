9bfb3e3e568438a3a9ac90c587b876868c307939
HongEC <honlucete@naver.com>
2024-12-29 01:40:30 +0900

    fix: PlanVO LocalDate로 수정

--- src/main/java/com/shinhan/daengdong/plan/vo/PlanVO.java
+++ src/main/java/com/shinhan/daengdong/plan/vo/PlanVO.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Builder
@@ -13,8 +14,8 @@ import java.time.LocalDateTime;
 public class PlanVO {
     private Long planId;             // 플랜 ID
     private String memberEmail;      // 작성자 이메일
-    private LocalDateTime startDate; // 시작 날짜
-    private LocalDateTime endDate;   // 종료 날짜
+    private LocalDate startDate; // 시작 날짜
+    private LocalDate endDate;   // 종료 날짜
     private int planState;           // 플랜 상태 (0: 비공개, 1: 공개)
     private LocalDateTime createAt;  // 생성일
     private LocalDateTime updateAt;  // 수정일
