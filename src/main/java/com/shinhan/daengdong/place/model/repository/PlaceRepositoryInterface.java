0ffcd09b4e2011699333d318e1af70ef8d08c0d5
HongEC <honlucete@naver.com>
2025-01-10 10:17:40 +0900

    feat: 여러 일정들을 저장하는 최종 여행계획 저장 기능 구현

---
+++ src/main/java/com/shinhan/daengdong/place/model/repository/PlaceRepositoryInterface.java
@@ -0,0 +1,10 @@
+package com.shinhan.daengdong.place.model.repository;
+
+import com.shinhan.daengdong.place.dto.PlaceDTO;
+import com.shinhan.daengdong.place.dto.PlanPlaceDTO;
+
+public interface PlaceRepositoryInterface {
+    void savePlace(PlaceDTO placeDTO);
+    void savePlanPlace(PlanPlaceDTO planPlaceDTO);
+
+}
